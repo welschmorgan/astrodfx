847f71f166856ee5d5b36b128f12eec0ad524ab5
Morgan Welsch <welschmorgan@gmail.com>
2020-09-05 13:34:22 +0200

    Improve Serializer

--- libs/core/Serializer.h
+++ libs/core/Serializer.h
@@ -9,8 +9,41 @@
 
 namespace quasar {
 	namespace core {
+		template<typename T, typename IStreamT = IStream>
+		class ISerializer {
+		public:
+			using value_type        = T;
+			using istream_type      = IStreamT;
+
+			ISerializer() = default;
+			ISerializer(const ISerializer &) = default;
+			virtual         ~ISerializer() noexcept = default;
+
+			ISerializer     &operator=(const ISerializer &) = default;
+
+			virtual void    read(istream_type &is, value_type &into) = 0;
+		};
+
+		template<typename T, typename OStreamT = OStream>
+		class OSerializer {
+		public:
+			using value_type        = T;
+			using ostream_type      = OStreamT;
+
+			OSerializer() = default;
+			OSerializer(const OSerializer &) = default;
+			virtual         ~OSerializer() noexcept = default;
+
+			OSerializer      &operator=(const OSerializer &) = default;
+
+			virtual void    write(ostream_type &os, const value_type &from) = 0;
+		};
+
 		template<typename T, typename IStreamT = IStream, typename OStreamT = OStream>
-		class Serializer {
+		class Serializer
+			: public ISerializer<T, IStreamT>
+			, public OSerializer<T, OStreamT>
+		{
 		public:
 			using value_type        = T;
 			using istream_type      = IStreamT;
@@ -21,9 +54,6 @@ namespace quasar {
 			virtual         ~Serializer() noexcept = default;
 
 			Serializer      &operator=(const Serializer &) = default;
-
-			virtual void    read(istream_type &is, value_type &into) = 0;
-			virtual void    write(ostream_type &os, const value_type &from) = 0;
 		};
 	}
 }
